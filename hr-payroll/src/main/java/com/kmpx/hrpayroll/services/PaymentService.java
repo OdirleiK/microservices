d9ca7ebe7a36f776575dd202183044fe9fb36df7
Odirlei Oliver Kempfer Siqueira Junior <odirlei67777@gmail.com>
2022-11-28 11:06:04 -0300

    Feign client

--- hr-payroll/src/main/java/com/kmpx/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/kmpx/hrpayroll/services/PaymentService.java
@@ -10,23 +10,19 @@ import org.springframework.web.client.RestTemplate;
 
 import com.kmpx.hrpayroll.entities.Payment;
 import com.kmpx.hrpayroll.entities.Worker;
+import com.kmpx.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
-	@Value("${hr-worker.host}")
-	private String workerHost;
+
 	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
 		
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
-		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
-		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
