465bdc326d86e6f46fd633c21196add3d910535d
Odirlei Oliver Kempfer Siqueira Junior <odirlei67777@gmail.com>
2022-11-29 10:23:36 -0300

    Configuring Eureka clients

--- hr-worker/src/main/java/com/kmpx/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/kmpx/hrworker/HrWorkerApplication.java
@@ -2,7 +2,9 @@ package com.kmpx.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
 
