c8c257e895ea1a7c3124b9a52e43c4cae4857a7b
Odirlei Oliver Kempfer Siqueira Junior <odirlei67777@gmail.com>
2022-11-29 14:17:34 -0300

    Configurin Zuul api gateway

--- hr-api-gateway-zuulf/src/main/java/com/kmpx/hrapigatewayzuul/HrApiGatewayZuulfApplication.java
+++ hr-api-gateway-zuulf/src/main/java/com/kmpx/hrapigatewayzuul/HrApiGatewayZuulfApplication.java
@@ -2,16 +2,14 @@ package com.kmpx.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
-public class HrApiGatewayZuulApplication {
+public class HrApiGatewayZuulfApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+		SpringApplication.run(HrApiGatewayZuulfApplication.class, args);
 	}
 
 }
